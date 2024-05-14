e2c8d6e032628aff11b374c585fec98eb214f9af
Phil Nocerini <nocerini@gmail.com>
2024-05-14 17:05:09 -0400

    code wars updates

--- paginationHelper.js
+++ paginationHelper.js
@@ -21,40 +21,41 @@
 
 class PaginationHelper {
 	constructor(collection, itemsPerPage) {
-	// The constructor takes in an array of items and a integer indicating how many
-	// items fit within a single page
-    this.collection = collection
-    this.itemsPerPage = itemsPerPage
+        // The constructor takes in an array of items and a integer indicating how many
+        // items fit within a single page
+        this.collection = collection
+        this.itemsPerPage = itemsPerPage
 	}
 	itemCount() {
 	// returns the number of items within the entire collection
-    return this.collection.length
+        return this.collection.length
 	}
 	pageCount() {
 	// returns the number of pages
-    return Math.ceil(this.itemCount() / this.itemsPerPage)
-	}
-	pageItemCount(pageIndex) {
-    // returns the number of items on the current page. page_index is zero based.
-	// this method should return -1 for pageIndex values that are out of range
-        let lastPageIDX = this.pageCount() - 1
-        if (pageIndex == lastPageIDX) {
-            return this.itemCount() % this.itemsPerPage
-        } else if (pageIndex < lastPageIDX && pageIndex >= 0) {
-            return this.itemsPerPage
-        } else {
-            return -1
-        }
+        return Math.ceil(this.itemCount() / this.itemsPerPage)
 	}
 	pageIndex(itemIndex) {
-	// determines what page an item is on. Zero based indexes
-	// this method should return -1 for itemIndex values that are out of range
-    if (itemIndex >= this.collection.length || itemIndex < 0) {
-        return -1
-    } else {
-        return Math.floor(itemIndex / this.itemsPerPage)
-    }
+        // determines what page an item is on. Zero based indexes
+        // this method should return -1 for itemIndex values that are out of range
+        if (itemIndex >= this.collection.length || itemIndex < 0) {
+            return -1
+        } else {
+            return Math.floor(itemIndex / this.itemsPerPage)
+        }
 	}
+    pageItemCount(pageIndex) {
+        // returns the number of items on the current page. page_index is zero based.
+        // this method should return -1 for pageIndex values that are out of range
+            let lastPageIDX = this.pageCount() - 1
+            let currentPage = this.pageIndex(itemIndex)
+            if (currentPage == lastPageIDX) {
+                return this.itemCount() % this.itemsPerPage
+            } else if (currentPage < lastPageIDX && currentPage >= 0) {
+                return this.itemsPerPage
+            } else {
+                return -1
+            }
+        }
 }
 
 const book = new PaginationHelper([4 , 6, 7, 10, 12, 16, 9], 3)
@@ -63,14 +64,15 @@ const book = new PaginationHelper([4 , 6, 7, 10, 12, 16, 9], 3)
 
 // console.log(book.itemsPerPage)
 
-// // console.log(book.itemCount())
+// console.log(book.itemCount())
 
-// // console.log(book.pageCount())
+// console.log(book.pageCount())
 
-// // console.log(book.pageItemCount(-1))
-// // console.log(book.pageItemCount(0))
-// // console.log(book.pageItemCount(2))
-// // console.log(book.pageItemCount(4))
+// console.log(book.pageItemCount(-1))
+// console.log(book.pageItemCount(0))
+// console.log(book.pageItemCount(4))
+// console.log(book.pageItemCount(6))
+// console.log(book.pageItemCount(9))
 
 // console.log(book.pageIndex(-2))
 // console.log(book.pageIndex(1))
@@ -79,6 +81,11 @@ const book = new PaginationHelper([4 , 6, 7, 10, 12, 16, 9], 3)
 // console.log(book.pageIndex(6))
 // console.log(book.pageIndex(10))
 
-const leaf = new PaginationHelper([], 1)
+const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+const helper = new PaginationHelper(collection, 10)
 
-console.log(leaf.pageItemCount(-1))
+console.log(helper.pageCount())
+
+console.log(helper.pageItemCount(2))
+console.log(helper.pageItemCount(3))
+console.log(helper.pageItemCount(4))
